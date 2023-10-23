b8d987a93cb1f7332f67c418768d3bc08562338f
erika-misheva <misevaerika@gmail.com>
2023-10-23 22:29:05 +0200

    banner bugs fix

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,35 +12,35 @@ export default function Banner() {
     const toRotate = ["Web Developer", "Frontend Developer", "Backend Developer"];
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 2000;
-    const tick = () => {
-        let i = loopNumber % toRotate.length;
-        let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-        setText(updatedText);
-
-        if (isDeleting) {
-            setDelta(prevDelta => prevDelta / 2)
-        }
-
-        if (!isDeleting && updatedText === fullText) {
-            setIsDeleting(true);
-            setDelta(period);
-        }
-        else if (isDeleting && updatedText === '') {
-            setIsDeleting(false);
-            setLoopNumber(loopNumber + 1);
-            setDelta(500)
-        }
-    }
-
     useEffect(() => {
+        const tick = () => {
+            let i = loopNumber % toRotate.length;
+            let fullText = toRotate[i];
+            let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+            setText(updatedText);
+    
+            if (isDeleting) {
+                setDelta(prevDelta => prevDelta / 2);
+            }
+    
+            if (!isDeleting && updatedText === fullText) {
+                setIsDeleting(true);
+                setDelta(period);
+            } else if (isDeleting && updatedText === '') {
+                setIsDeleting(false);
+                setLoopNumber(loopNumber + 1);
+                setDelta(500);
+            }
+        };
+    
         let ticker = setInterval(() => {
-            tick()
-        }, delta)
-        return () => { clearInterval(ticker) }
-    },
-        [text, delta, tick])
-
+            tick();
+        }, delta);
+    
+        return () => {
+            clearInterval(ticker);
+        };
+    }, [text, delta]);
 
     return (
         <section className="banner" id="home">
@@ -66,7 +66,7 @@ export default function Banner() {
                         </TrackVisibility>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        <img src={headerImage} alt="Header Image" />
+                        <img src={headerImage} alt="Header" />
                     </Col>
                 </Row>
             </Container>
